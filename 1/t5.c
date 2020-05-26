33bfbea6db33ab627f1eba3ff3eab221717b9632
hga38 <richard_gao@sfu.ca>
2020-05-26 12:45:50 -0700

    t5.c run success, submitting

---
+++ 1/t5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  // changable scan value per line
+  double scan_value = 0;
+  int scan_valid = 0;
+  // scan loop
+  while (scan_valid != EOF) {
+    scan_valid = scanf("%lf[^\n]%*c", &scan_value);
+    if (scan_valid == 1) {
+      int val_ceil = (int) ceil(scan_value);
+      int val_floor = (int) floor(scan_value);
+      int val_round = (int) round(scan_value);
+
+      printf(
+        "%d %d %d\n",
+        val_floor,
+        val_round,
+        val_ceil);
+    } else {
+      scan_value = 0;
+    }
+  }
+  // print done when EOF reached
+  printf("Done.\n");
+  return 0;
+}
