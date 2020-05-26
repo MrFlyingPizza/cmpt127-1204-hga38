f6133eb5f7c4918b26e40aff27af35c26269f21a
hga38 <richard_gao@sfu.ca>
2020-05-26 10:40:01 -0700

    t4.c run success, submitting

---
+++ 1/t4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+void computeFloat(float val) {
+  // print the floor ceiling and rounding for the input val
+  int val_ceil = (int) ceil(val);
+  int val_floor = (int) floor(val);
+  int val_round = (int) round(val);
+  printf("%d %d %d", val_floor, val_round, val_ceil);
+}
+
+int main(void) {
+  double input_val = 0;
+  int input_valid = scanf("%lf", &input_val);
+  // input validation
+  if (input_val == 0) {
+    printf("scanf error: (%d)\n", input_valid);
+  } else {
+    computeFloat(input_val);
+  }
+
+  return 0;
+}
