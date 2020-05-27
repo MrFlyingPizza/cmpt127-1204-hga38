e58a84e738c2a78f1130373e10d7c33dcd05ddc8
hga38 <richard_gao@sfu.ca>
2020-05-27 00:38:16 -0700

    t8.c run success, submitting

---
+++ 1/t8.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+  // init values
+  short scan_result = 1;
+  unsigned short scan_value;
+  unsigned short dimensions[3];
+  // loop 3 times to get 3 positive values
+  for (unsigned short i = 0; i < 3; i++) {
+    if (scan_result == 1) {
+      scan_result = scanf("%hu%*[ ]%*[\n]", &scan_value);
+      dimensions[i] = scan_value;
+    } else {
+      printf("Failed");
+      return 1;
+    }
+  }
+
+
+
+
+  // 2D array construction
+  unsigned short length = dimensions[1];
+  unsigned short width = dimensions[0];
+  // unsigned short height = dimensions[2];
+  unsigned short length_i_final = length - 1;
+  unsigned short width_i_final = width - 1;
+  char building_block;
+
+  // construction loops
+  for (unsigned short length_i = 0; length_i < length; length_i++) { // length construction
+
+    for (unsigned short width_i = 0; width_i < length; width_i++) { // width construction
+      // if statement to check for first width and last width
+      if (length_i == 0 ||
+        length_i == length_i_final ||
+         width_i == 0 ||
+         width_i == width_i_final
+       ) {
+        building_block = '#';
+      } else {
+        building_block = '.';
+      }
+      // print each char
+      printf("%c", building_block);
+    }
+    printf("\n");
+  }
+}
