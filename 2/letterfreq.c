d3c5d64fc5d3e3e82bbe9d071ed949ec2c7d55bd
hga38 <richard_gao@sfu.ca>
2020-06-06 16:57:51 -0700

    letterfreq.c functional.

---
+++ 2/letterfreq.c
@@ -0,0 +1,25 @@
+// letterfreq.c
+// counts the frequency of each letter in input stream
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+  char ch;
+  double alpha[26];
+  unsigned int total = 0;
+  while ((ch = getchar()) != EOF) {
+    if (ch >= 97 && ch <= 122) {
+      total++;
+      alpha[ch-97]++;
+    } else if (ch >= 65 && ch <= 90) {
+      total++;
+      alpha[ch-65]++;
+    }
+  }
+
+  for (unsigned short alpha_i = 0; alpha_i < 26; alpha_i++) {
+    printf("%c %.4f\n", 97 + alpha_i, alpha[alpha_i]/total);
+  }
+  return 0;
+}
