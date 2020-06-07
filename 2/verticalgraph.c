c35cc649f3a2b2ec19ec448950afca99b73fe02b
hga38 <richard_gao@sfu.ca>
2020-06-06 18:01:10 -0700

    verticalgraph.c functional.

---
+++ 2/verticalgraph.c
@@ -0,0 +1,50 @@
+// verticalgraph.c
+// graphs stdin values
+
+#include <stdio.h>
+
+int searchHighest(int* values, int len) { // find highest value
+  int highest_value = 0;
+  for (unsigned int len_i = 0; len_i < len; len_i++) {
+    if (values[len_i] > highest_value) {
+      highest_value = values[len_i];
+    }
+  }
+  return highest_value;
+}
+
+
+int main(int argc, char const *argv[]) {
+  int values[80];
+  for (unsigned short values_i = 0; values_i < 80; values_i++) {
+    values[values_i] = 0;
+  }
+  int scan_value;
+
+  unsigned short scan_amount = 0;
+  while (scanf("%d%*[ ]%*[\n]", &scan_value) != EOF && scan_amount < 80) { // scan values
+    values[scan_amount] = scan_value;
+    scan_amount++;
+  }
+
+  int highest_value = searchHighest(values, 80);
+  int abs_highest = highest_value;
+  printf("%d", highest_value);
+  char point;
+  for (int range_i = 0; range_i < abs_highest; range_i++) {
+
+    highest_value = searchHighest(values, 80);
+    for (int domain_i = 0; domain_i < scan_amount; domain_i++) {
+      if (values[domain_i] == highest_value) {
+        point = '#';
+        values[domain_i]--;
+      } else {
+        point = ' ';
+      }
+      printf("%c", point);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
