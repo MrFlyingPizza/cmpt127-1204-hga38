c395a9b8cabfc5a64565a041baf01b92cf26d11a
hga38 <richard_gao@sfu.ca>
2020-06-08 21:26:00 -0700

    Fixed indentation.

--- 2/censored.c
+++ 2/censored.c
@@ -5,75 +5,65 @@
 
 // this works on my computer but not the grading robot I cant figure out why
 
-int main(int argc, char const *argv[])
-{
-  // print parameters
-  // for (int argarg = 0; argarg < argc; argarg++) {
-  //   printf("%s\n", argv[argarg]);
-  // }
+int main(int argc, char const *argv[]) {
+	// print parameters
+	// for (int argarg = 0; argarg < argc; argarg++) {
+	//   printf("%s\n", argv[argarg]);
+	// }
 
-  // calc argument lengths
-  unsigned int argLengths[argc];
-  for (unsigned int argLen_i; argLen_i < argc; argLen_i++)
-  {
-    argLengths[argLen_i] = strlen(argv[argLen_i]);
-  }
+	// calc argument lengths
+	unsigned int argLengths[argc];
+	for (unsigned int argLen_i; argLen_i < argc; argLen_i++) {
+		argLengths[argLen_i] = strlen(argv[argLen_i]);
+	}
 
-  // censor substrings
-  char ch = ' ';
-  char buffer[128] = {0};
+	// censor substrings
+	char ch = ' ';
+	char buffer[128] = {0};
 
-  unsigned int arg_i = 0;
-  unsigned short found = 0;
+	unsigned int arg_i = 0;
+	unsigned short found = 0;
 
-  unsigned int argCh_i = 0;
-  unsigned short argMatch = 1;
+	unsigned int argCh_i = 0;
+	unsigned short argMatch = 1;
 
-  while ((ch = getchar()) != EOF)
-  {
-    //printf("Current ch: %c\n", ch);
-    arg_i = 0;
-    found = 0;
-    while (arg_i < argc && found == 0 && ch != EOF)
-    { //
-      //printf("arg_i: %u\n", arg_i);
-      argMatch = 1;
-      argCh_i = 0;
-      while (argMatch == 1 && argCh_i < argLengths[arg_i] && ch != EOF)
-      {
+	while ((ch = getchar()) != EOF) {
+		//printf("Current ch: %c\n", ch);
+		arg_i = 0;
+		found = 0;
+		while (arg_i < argc && found == 0 && ch != EOF) { //
+		//printf("arg_i: %u\n", arg_i);
+		argMatch = 1;
+		argCh_i = 0;
+		while (argMatch == 1 && argCh_i < argLengths[arg_i] && ch != EOF) {
 
-        if (ch == argv[arg_i][argCh_i])
-        {
-          buffer[argCh_i] = ch;
-          ch = getchar();
-          argCh_i++;
-        }
-        else
-        {
-          argMatch = 0;
-          for (unsigned short buffer_i = 0; buffer_i < argCh_i; buffer_i++)
-          {
-            printf("%c", buffer[buffer_i]);
-          }
+			if (ch == argv[arg_i][argCh_i]) {
+			buffer[argCh_i] = ch;
+			ch = getchar();
+			argCh_i++;
+			} else {
+			argMatch = 0;
+			for (unsigned short buffer_i = 0; buffer_i < argCh_i; buffer_i++) {
+				printf("%c", buffer[buffer_i]);
+			}
+			}
 
-        }
+		}
 
-      }
+		if (argMatch == 1) {
+			found = 1;
+		} else {
+			found = 0;
+		}
 
-      if (argMatch == 1)
-      {
-        found = 1;
-      }
+		arg_i++;
+		}
 
-      arg_i++;
-    }
+		if (found == 1) {
+		printf("CENSORED");
+		}
+		printf("%c", ch);
 
-    if (found == 1)
-    {
-      printf("CENSORED");
-    }
-    printf("%c", ch);
-
-  }
-  return 0;
+	}
+	return 0;
 }
