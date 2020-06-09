4951d6cf7ffb42f29b1b2d1ae2f8dc82c30da276
hga38 <richard_gao@sfu.ca>
2020-06-09 09:26:55 -0700

    Added exercise3.c

--- exercises/exercise3.c
+++ exercises/exercise3.c
@@ -1,3 +1,11 @@
+/*
+  exercise3.c
+  prompts user for floating point input and computes
+  the floor, rounded and ceiling of the value.
+  By Richard(Han), Gao
+  June 09, 2020
+*/
+
 #include <stdio.h>
 #include <math.h>
 
@@ -7,17 +15,18 @@ void computeFloat(float value)
   int val_ceil = (int) ceil(value);
   int val_floor = (int) floor(value);
   int val_round = (int) round(value);
-  printf("%d %d %d\n", val_floor, val_round, val_ceil);
+  printf("Floor of the value: \t%d\nRounded to integer: \t%d\nCeiling of the value: \t%d\n", val_floor, val_round, val_ceil);
 }
 
 int main(void)
 {
   double input_value = 0;
+  printf("Enter a valid float point value. (In range of Double)\n=> "); // prompt for input
   int input_valid = scanf("%lf", &input_value);
   // input validation
   if (input_value == 0)
   {
-    printf("scanf error: (%d)\n", input_valid);
+    printf("Input invalid! Scanf error: (%d)\n", input_valid);
   }
   else
   {
