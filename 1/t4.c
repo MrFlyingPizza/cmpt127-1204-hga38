afb0bf867ccc6dbcfc8f55099fc8f77672c2be56
hga38 <richard_gao@sfu.ca>
2020-06-13 11:41:22 -0700

    reformatted t4.c

--- 1/t4.c
+++ 1/t4.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 
-void computeFloat(float val) {
+void computeFloat(float value)
+{
   // print the floor ceiling and rounding for the input val
-  int val_ceil = (int) ceil(val);
-  int val_floor = (int) floor(val);
-  int val_round = (int) round(val);
+  int val_ceil = (int) ceil(value);
+  int val_floor = (int) floor(value);
+  int val_round = (int) round(value);
   printf("%d %d %d\n", val_floor, val_round, val_ceil);
 }
 
-int main(void) {
-  double input_val = 0;
-  int input_valid = scanf("%lf", &input_val);
+int main(void)
+{
+  double input_value = 0;
+  int input_valid = scanf("%lf", &input_value);
   // input validation
-  if (input_val == 0) {
+  if (input_value == 0)
+  {
     printf("scanf error: (%d)\n", input_valid);
-  } else {
-    computeFloat(input_val);
+  }
+  else
+  {
+    computeFloat(input_value);
   }
 
   return 0;
