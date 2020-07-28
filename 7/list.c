fb73e1215dc52c3532aa328de41a44d9fed23023
hga38 <richard_gao@sfu.ca>
2020-07-28 10:31:53 -0700

    new implementation of task 7

--- 7/list.c
+++ 7/list.c
@@ -7,87 +7,128 @@
 
 list_t* list_create( void )
 {
-  list_t* l = malloc( sizeof(list_t) );
-  if( l )
-    {
-      l->head = NULL;
-      l->tail = NULL;
-    }
-  return l;
+	list_t* new_list = malloc(sizeof(list_t));
+	if (new_list)
+	{
+		new_list->head = NULL;
+		new_list->tail = NULL;
+	}
+	return new_list;
 }
 
 void list_destroy( list_t* list )
 {
-  element_t* el = list->head;
-  while( el )
-    {
-      element_t* next = el->next;
-      free( el );
-      el = next;
-    }      
-  
-  free( list );
+	if (list == NULL)
+	{
+		return;
+	}
+	
+	element_t* el = list->head, *next;
+	while (el)
+	{
+		next = el->next;
+		free(el);
+		el = next;
+	}
+	
 }
 
 element_t* element_create( int i )
 {
-  element_t* el = malloc( sizeof(element_t) );
-  if( el )
-    {
-      el->val = i;
-      el->next = NULL;
-    }
-  return el;
+	element_t* el = malloc(sizeof(element_t));
+	if (el)
+	{
+		el->next = NULL;
+		el->val = i;
+	}
+
+	return el;
 }
 
 int list_append( list_t* list, int i )
 {
-  element_t* el = element_create( i );
-  if( el == NULL )
-    return 1;
+	if (list == NULL)
+	{
+		return 1;
+	}
 
-  if( list->head == NULL )
-    list->head = el;
-  
-  if( list->tail )
-    list->tail->next = el;
+	element_t* el = malloc(sizeof(element_t));
+	if (el == NULL)
+	{
+		return 1;
+	}
 
-  list->tail = el;
-  return 0;
+	el->val = i;
+	el->next = NULL;
+
+	if (list->head == NULL)
+	{
+		list->head = el;
+		list->tail = el;
+	}
+	else
+	{
+		if (list->tail == NULL)
+		{
+			return 1;
+		}
+		
+
+		list->tail->next = el;
+		list->tail = el;
+	}
+	
+	return 0;
+	
 }
 
 int list_prepend( list_t* list, int i )
 {
-  element_t* el = element_create( i );
-  if( el == NULL )
-    return 1;
+	if (list == NULL)
+	{
+		return 1;
+	}
 
-  if( list->tail == NULL )
-    list->tail = el;
-  
-  if( list->head )
-    el->next = list->head;
+	element_t* el = malloc(sizeof(element_t));
+	if (el == NULL)
+	{
+		return 1;
+	}
 
-  list->head = el;
-  
-  return 0;
+	el->val = i;
+	el->next = NULL;
+
+	el->next = list->head->next;
+	list->head = el;
+	return 0;
 }
 
 element_t* list_index( list_t* list, unsigned int i )
 {
-  if( list->head == NULL )
-    return NULL;
-  
-  element_t* el = list->head;
-  unsigned int now = 0;
-  
-  while( now < i )
-    {
-      now++;
-      el = el->next;
-    }     
-  
-  return el;
+	if (list == NULL)
+	{
+		return NULL;
+	}
+
+	if (list->head == NULL || list->tail == NULL)
+	{
+		return NULL;
+	}
+	
+	
+	unsigned int count = 0;
+	element_t* result = NULL, *el = list->head;
+	while (el != NULL && i < count)
+	{
+		if (count == i)
+		{
+			return el;
+		}
+		el = el->next;
+		++count;
+	}
+	
+	return el;
 }
 
 void list_print( list_t* list )
