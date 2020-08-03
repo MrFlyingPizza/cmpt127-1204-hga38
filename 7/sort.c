69c7da0ee884204cce10aace551ac61969c62b19
hga38 <richard_gao@sfu.ca>
2020-08-03 13:53:03 -0700

    clear old broken code

--- 7/sort.c
+++ 7/sort.c
@@ -2,139 +2,89 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// returns list size
-int list_size(list_t* list)
+void append(list_t* list, element_t* el)
 {
-    if (list == NULL) return -1;
-    if (list->head == NULL) return 0;
     
-    int size = 1;
-    element_t *el = list->head;
-    while (el->next != NULL)
-    {
-        size += 1;
-        el = el->next;
-    }
-    
-    return size;
 }
 
-// void arr_print(int arr[], unsigned int len)
-// {
-//     printf("[ ");
-//     for (unsigned int i = 0; i < len; i++)
-//     {
-//         printf("%d", arr[i]);
-//         (i != len-1) ? printf(", ") : printf("");
-//     }
-//     printf(" ]\n");
-// }
-
-void merge(list_t* list, int left, int right, int mid)
+void merging(element_t* left_l, element_t* left_r, element_t* right_l, element_t* right_r)
 {
-    element_t *head_l = list_index(list, left);
-    element_t *head_r = list_index(list, mid + 1);
-    element_t *el_l = head_l, *el_r = head_r;
-
-    int merge_arr[right - left + 1];
-
-    unsigned int count_l = 0, count_r = 0, size_l, size_r;
-    size_l = mid - left + 1;
-    size_r = right - mid;
-
-    int rel_i = 0; // relative indexing
-    while (count_l < size_l && count_r < size_r)
+    int temp_val = 0;
+    element_t *temp_el_1 = NULL, *temp_el_2 = NULL;
+    while (left_l != left_r->next && right_l != right_r->next)
     {
-        if (el_l->val <= el_r->val)
+        if (left_l->val < right_l->val)
         {
-            merge_arr[rel_i] = el_l->val;
-            el_l = el_l->next;
-            count_l++;
+            left_l = left_l->next;
         }
         else
         {
-            merge_arr[rel_i] = el_r->val;
-            el_r = el_r->next;
-            count_r++;
-        }
-        rel_i++;
-    }
+            temp_val = left_l->val;
+            left_l->val = right_l->val;
+            right_l->val = temp_val;
 
-    unsigned int current_count, size;
-    element_t *el;
-    if (count_l < size_l)
-    {
-        el = el_l;
-        current_count = count_l;
-        size = size_l;
-    }
-    else
-    {
-        el = el_r;
-        current_count = count_r;
-        size = size_r;
-    }
-    
-    for (unsigned int i = current_count; i < size; i++)
-    {
-        merge_arr[rel_i] = el->val;
-        el = el->next;
-        rel_i++;
+            temp_el_1 = left_l->next;
+            left_l->next = right_l;
+
+            temp_el_2 = right_l->next;
+            right_l->next = temp_el_1;
+
+            temp_el_1->next = temp_el_2;
+
+            right_l = temp_el_2;
+        }
+        
+        
     }
     
-    
-    el = head_l;
-    for (unsigned int i = 0; i < size_l + size_r; i++)
-    {
-        el->val = merge_arr[i];
-        el = el->next;
-        //if (el == NULL) puts("!! null reached el");
-    }
-    //arr_print(merge_arr, right - left + 1);
 }
 
-void sort(list_t* list, int left, int right)
+void sorting(element_t* left, element_t* right)
 {
-    if (left >= right) return;
+    element_t *moving_left = left, *moving_right = right;
+    while (moving_right != right)
+    {
+        moving_right = moving_right->next;
+        if (moving_right != right)
+        {
+            moving_left = moving_left->next;
+            moving_right = moving_right->next;
+        }
 
-    int mid = (left + right) / 2;
-    sort(list, left, mid);
-    sort(list, mid + 1, right);
+    }
 
-    merge(list, left, right, mid);
-    
+    sorting(left, moving_left);
+    sorting(moving_left->next, right);
+
+    //merging(moving_left->next, right);
 }
 
 void list_sort(list_t* list)
 {
-    if (list == NULL) return;
-    if (list->head == NULL) return;
-    
     unsigned int size = list_size(list);
-    unsigned int highest_index = size - 1;
-    sort(list, 0, highest_index);
+    printf("size: %u\n", size);
 }
 
-// int main()
-// {
-//     list_t *list = list_create();
-//     int len = 10000;
-//     for (int i = 0; i < len; i++)
-//     {
-//         list_append(list, rand());
-//     }
-//     puts("testing...");
-//     list_sort(list);
-//     list_print(list);
+int main()
+{
+    list_t *list = list_create();
+    int len = 100000;
+    for (int i = 0; i < len; i++)
+    {
+        list_append(list, rand());
+    }
+    puts("testing...");
+    list_sort(list);
+    //list_print(list);
 
-//     for (int i = 0; i < len - 1; i++)
-//     {
-//         if (list_index(list, i) > list_index(list, i + 1))
-//         {
-//             printf("Failed check at index: %d", i);
-//         }
+    for (int i = 0; i < len - 1; i++)
+    {
+        if (list_index(list, i) > list_index(list, i + 1))
+        {
+            printf("Failed check at index: %d", i);
+        }
         
-//     }
+    }
     
-//     return 0;
-// }
+    return 0;
+}
