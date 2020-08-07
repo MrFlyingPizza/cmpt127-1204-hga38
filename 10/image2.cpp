98b8e42cf27c448aaabfc1f843975148062d6806
hga38 <richard_gao@sfu.ca>
2020-08-07 08:36:03 -0700

    added save and load functiosn

--- 10/image2.cpp
+++ 10/image2.cpp
@@ -2,6 +2,84 @@
 #include <iostream>
 #include <fstream>
 
+
+/* Changes the size of an image, allocating memory as necessary, and
+    setting all pixels to fillcolour. Returns 0 on success, or a
+    non-zero error code.*/
+int Image::resize(unsigned int width, unsigned int height, uint8_t fillcolour)
+{
+    try
+    {
+        delete[] pixels;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+
+    try
+    {
+        pixels = new uint8_t[width*height*sizeof(uint8_t)];
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 2;
+    }
+    
+    this->cols = width;
+    this->rows = height;
+
+    return 0;
+}
+
+/* Sets the colour of the pixel at (x,y) to colour. Returns 0 on
+    success, else a non-zero error code. If (x,y) is not a valid
+    pixel, the call fails and the image does not change.*/
+int Image::set_pixel(unsigned int x, unsigned int y, uint8_t colour)
+{
+    if (x > cols || y > rows)
+    {
+        std::cerr << "Pixel target out of bounds.\n";
+        return 1;
+    }
+    
+    try
+    {
+        pixels[y * cols + x] = colour;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 2;
+    }
+    return 0;
+}
+
+/* Gets the colour of the pixel at (x,y) and stores at the address
+    pointed to by colourp. Returns 0 on success, else a non-zero error
+    code. */
+int Image::get_pixel(unsigned int x, unsigned int y, uint8_t *colourp)
+{
+    if (x > cols || y > rows)
+    {
+        std::cerr << "Pixel target out of bounds.\n";
+        return 1;
+    }
+
+    try
+    {
+        *colourp = pixels[y * cols + x];
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 2;
+    }
+    return 0;
+}
+
 /* Saves the image in the file filename. In a format that can be
     loaded by load(). Returns 0 on success, else a non-zero error
     code. */
