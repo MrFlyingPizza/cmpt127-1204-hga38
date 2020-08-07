c3ae261ed632d7d57ca827b5ea9c71d80d9681cd
hga38 <richard_gao@sfu.ca>
2020-08-07 09:37:44 -0700

    finished save and load

--- 10/image3.cpp
+++ 10/image3.cpp
@@ -54,7 +54,67 @@ int Image::set_pixel(unsigned int x, unsigned int y, uint8_t colour)
         return 1;
     }
 
+    pixels[y][x] = colour;
     
-    
+    return 0;
+}
+
+int Image::get_pixel(unsigned int x, unsigned int y, uint8_t* colourp)
+{
+    if (x >= cols || y >= rows)
+    {
+        std::cerr << "Coordinate out of bounds!" << std::endl;
+        return 1;
+    }
+
+    *colourp = pixels[y][x];
+
+    return 0;
+}
+
+int Image::save(const char* filename)
+{
+    std::ofstream file(filename, std::ios::out | std::ios::binary);
+    if (file.is_open())
+    {
+        for (unsigned int i = 0; i < rows; i++)
+        {
+            for (unsigned int j = 0; j < cols; j++)
+            {
+                file << pixels[i][j];
+            }
+            
+        }
+        file.close();
+    }
+    else
+    {
+        std::cout << "File not open to writing.\n";
+        return 1;
+    }
+    return 0;
+}
+
+int Image::load(const char* filename)
+{
+    try
+    {
+        std::ifstream file(filename, std::ios::in | std::ios::binary);
+        for (unsigned int i = 0; i < rows; i++)
+        {
+            for (unsigned int j = 0; j < cols; j++)
+            {
+                file >> pixels[i][j];
+            }
+            
+        }
+        file.close();
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "failed to load image." << std::endl;
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
     return 0;
 }
