f57d0a4004a031bd80c76c60f0618017782ee709
hga38 <richard_gao@sfu.ca>
2020-08-18 22:11:10 -0700

    work

--- 7/sort.c
+++ 7/sort.c
@@ -11,43 +11,41 @@ unsigned int list_size(list_t* list)
         ++size;
         el = el->next;
     }
-    
+
     return size;
 }
 
 list_t* list_merge(list_t* list_a, list_t* list_b)
 {
     list_t* result = list_create();
-    element_t *el_a, *el_b;
+    element_t *el_a = list_a->head, *el_b = list_b->head;
+    
     while (el_a != NULL && el_b != NULL)
     {
         if (el_a->val < el_b->val)
         {
-            list_append(result, el_a->val);
+            if (list_append(result, el_a->val) == 1) puts("it no work");
             el_a = el_a->next;
         }
         else
         {
-            list_append(result, el_b->val);
+            if (list_append(result, el_b->val) == 1) puts("it no work");
             el_b = el_b->next;
         }
         
     }
-    
+
     while (el_a != NULL)
     {
-        list_append(result, el_a->val);
+        if (list_append(result, el_a->val) == 1) puts("it no work");
         el_a = el_a->next;
     }
     
     while (el_b != NULL)
     {
-        list_append(result, el_b->val);
+        if (list_append(result, el_b->val) == 1) puts("it no work");
         el_b = el_b->next;
     }
-
-    list_destroy(list_a);
-    list_destroy(list_b);
 
     return result;
 }
@@ -68,15 +66,16 @@ void list_sort(list_t* list)
     unsigned int middle = size/2;
     
     list_t *list_a = list_create(), *list_b = list_create();
+    
     for (unsigned int i = 0; i < size; i++)
     {
         if (i < middle)
         {
-            list_append(list_a, list_index(list, i)->val);
+            if (list_append(list_a, list_index(list, i)->val) != 0) puts("it no work");
         }
         else
         {
-            list_append(list_b, list_index(list, i)->val);
+            if (list_append(list_b, list_index(list, i)->val) != 0) puts("it no work");
         }
         
     }
@@ -86,29 +85,35 @@ void list_sort(list_t* list)
 
     list_t* result = list_merge(list_a, list_b);
     
-    list = result;
+    list_destroy(list);
+    list = list_create();
+
+    list->head = result->head;
+    list->tail = result->tail;
 }
 
-// int main()
-// {
-//     list_t *list = list_create();
-//     int len = 10;
-//     for (int i = 0; i < len; i++)
-//     {
-//         list_append(list, rand());
-//     }
-//     puts("testing...");
-//     list_sort(list);
-//     //list_print(list);
+int main()
+{
+    list_t *list = list_create();
+    int len = 7;
+    for (int i = 0; i < len; i++)
+    {
+        list_append(list, rand() % 1000);
+    }
+    puts("testing...");
+    list_sort(list);
 
-//     for (int i = 0; i < len - 1; i++)
-//     {
-//         if (list_index(list, i) > list_index(list, i + 1))
-//         {
-//             printf("Failed check at index: %d", i);
-//         }
+    for (int i = 0; i < len - 1; i++)
+    {
+        printf("CHECKING INDEX: %d\n", i);
+        if (list_index(list, i)->val > list_index(list, i + 1)->val)
+        {
+            printf("Failed check at index: %d\n", i);
+        }
         
-//     }
+    }
+
+    list_print(list);
     
-//     return 0;
-// }
+    return 0;
+}
